66d432d3655036e85f493ab5a002b8d9eca22ddb
Charlotte House <cdmhouse@sky.com>
2020-05-16 18:31:36 +0100

    previously tested code added

--- reverse-the-word-not-the-sentence/src/Components/1st Level/ActualOutput.js
+++ reverse-the-word-not-the-sentence/src/Components/1st Level/ActualOutput.js
@@ -2,6 +2,26 @@ import React from 'react';
 
 class ActualOutput  extends React.Component {
 
+    function splitToArray(phrases) {
+
+        return arrayWordOutput = phrases.split(" ");  
+        }
+        
+        // 3. split the letters in the array
+        
+        function splitToArrayWords(arrayWordOutput) {
+        
+            return arrayLetterOutput = arrayWordOutput.split("");  
+            }
+        //4. Reverse the letter in each individual array 
+        
+        function reverser(arrayLetterOutput){
+            return reversed = arrayLetterOutput.reverse();
+        }
+        
+        //5. Join the arrays back together
+        function wordBackTogether(reversed){
+            return words = reversed.join("");
    
 render(){
     return (
