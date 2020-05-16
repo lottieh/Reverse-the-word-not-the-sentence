3255cf0c7bd91f2febc52ffb26c704537d19e19f
Charlotte House <cdmhouse@sky.com>
2020-05-16 17:16:46 +0100

    working on input section

---
+++ reverse-the-word-not-the-sentence/src/Components/1st Level/InputTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function InputTitle() {
+    
+    return (
+
+      <h3 className="col-m-4">
+       Please input your phrase here:
+      </h3>
+
+    );
+  }
+export default InputTitle;
