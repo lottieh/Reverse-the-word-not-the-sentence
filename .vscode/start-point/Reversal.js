fbed132239f0ffd747b811799d68dd40d6047d6e
Charlotte House <cdmhouse@sky.com>
2020-05-16 11:51:15 +0100

    splitToArray added

--- .vscode/start-point/Reversal.js
+++ .vscode/start-point/Reversal.js
@@ -1,8 +1,16 @@
-const phrases = []; 
+const phrases = (cheese); 
 
 //1. Take in a phrase string
 
 //2. Split string into array for each word - space as the delimiter
+
+function splitToArray(phrase) {
+const string = phrase
+
+arrayOutput=string.split();  
+}
+console.log(arrayOutput);
+
 
 //3. Reverse the letter in each individual array 
 
---
+++ .vscode/start-point/Reversal.test.js
@@ -0,0 +1,13 @@
+const wordReversal = require("./Reversal")
+
+
+describe("wordReversal", () => {
+  describe("wordReversal.splitToArray", () => {
+    test("Returns the given string as arrays", () => {
+      expect(phrases.splitToArray()).toBe(cheese);
+    });
+  });
+});
+
+
+
