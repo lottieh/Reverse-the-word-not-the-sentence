21ccbf4f900b67062665c5c6dcc2a2176a79003e
Charlotte House <cdmhouse@sky.com>
2020-05-16 16:15:29 +0100

    create Header component and import to App

---
+++ reverse-the-word-not-the-sentence/src/Components/1st Level/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Header() {
+    
+    return (
+
+      <h1 className="col-m-4">
+       The Word Reversal Application
+      </h1>
+
+    );
+  }
+export default Header;
