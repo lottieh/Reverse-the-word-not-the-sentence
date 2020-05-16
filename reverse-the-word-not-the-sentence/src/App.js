a6113752f3d7768a96fe71c6f9a809364044ac34
Charlotte House <cdmhouse@sky.com>
2020-05-16 16:46:30 +0100

    Explanation component written and imported

--- reverse-the-word-not-the-sentence/src/App.js
+++ reverse-the-word-not-the-sentence/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/1st Level/Header';
+import Explanation from './Components/1st Level/Explanation';
 
 class App extends React.Component {
   render(){
@@ -10,8 +11,8 @@ class App extends React.Component {
      
 
         <Header/>
-        
-        
+        <Explanation/>
+
     
     </div>
   );
---
+++ reverse-the-word-not-the-sentence/src/Components/1st Level/Explanation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function Explanation() {
+    
+    return (
+
+      <h3 className="col-m-4">
+       The purposes of this application is to  reverse the letters of a word in a string  but leaving 
+the words in the same order.
+<br>
+</br>
+The following input can take a  string of N characters in length consisting of lowercase letters (a-z) and spaces only. 
+
+      </h3>
+
+    );
+  }
+export default Explanation;
