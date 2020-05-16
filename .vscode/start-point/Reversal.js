bbe27e8d329481788e7bb19efac3d2a4cd16e817
Charlotte House <cdmhouse@sky.com>
2020-05-16 11:43:07 +0100

    code layout

---
+++ .vscode/start-point/Reversal.js
@@ -0,0 +1,16 @@
+const phrases = []; 
+
+//1. Take in a phrase string
+
+//2. Split string into array for each word - space as the delimiter
+
+//3. Reverse the letter in each individual array 
+
+//4. Join the arrays back together
+
+//5. Output the final phrase
+
+
+
+
+
