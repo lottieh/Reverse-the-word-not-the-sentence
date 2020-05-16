27b6c78620cb14f39978f10758bffd134375001e
Charlotte House <cdmhouse@sky.com>
2020-05-16 22:37:10 +0100

    comment added

--- .vscode/Final.js
+++ .vscode/Final.js
@@ -1,11 +1,11 @@
-const phrases = "cheese is the best"; 
-
 //1. Take in a phrase string
+const phrases = "cheese is the best"; 
 
 //2. Split string into array for each word - space as the delimiter
 
 let  arrayWordOutput = phrases.split(" ");  
 console.log(arrayWordOutput)
+// need to change this to not be an array
 
 // 3. split the letters in the array
 let arrayLetterOutput = arrayWordOutput.split(" ");  
